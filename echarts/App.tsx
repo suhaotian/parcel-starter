446ee142cecf20d8ad3fa260af2f67ff4dad0912
suhaotian <h4av.one@gmail.com>
2021-11-16 21:28:55 +0800

    add echart demo

---
+++ echarts/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Echart from './Echart'
+
+
+export default function App() {
+  return <Echart style={{
+    width: '100%',
+    height: '100vh',
+  }} option={{
+    title: {
+      text: 'ECharts 入门示例'
+    },
+    tooltip: {},
+    xAxis: {
+      data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+    },
+    yAxis: {},
+    series: [
+      {
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }
+    ]
+  }} />
+}
---
+++ echarts/Echart.tsx
@@ -0,0 +1,56 @@
+import React, { useState, useRef, useEffect, useMemo, useLayoutEffect } from 'react'
+import echarts, { ECOption } from './setup';
+
+export type EchartProp = {
+  option: ECOption,
+  key?: string,
+  style?: {
+    width: string,
+    height: string
+  }
+  className?: string
+}
+
+const removeUndefined = (obj: object) => {
+  for (let key in obj) {
+    if (obj[key as keyof typeof obj] === undefined) {
+      delete obj[key as keyof typeof obj]
+    }
+  }
+  return obj
+}
+
+const Echart: React.FC<EchartProp> = ({ option, key, className, style = { width: '100%', height: '100%' } }) => {
+
+  const chartRef = useRef<HTMLDivElement>(null)
+  const [echartsInstance, setEchartsInstance] = useState<echarts.ECharts>()
+
+  useLayoutEffect(() => {
+    setEchartsInstance(echarts.init(chartRef.current as HTMLDivElement))
+  }, [])
+
+  useEffect(() => {
+    const handleResize = () => {
+      echartsInstance?.resize()
+    }
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [echartsInstance])
+
+  useEffect(() => {
+    echartsInstance?.setOption(option)
+  }, [echartsInstance, option])
+
+
+  const obj = useMemo(() => {
+    return removeUndefined({ option, key, className, style });
+  }, [option, key, className, style])
+
+  return (
+    <div ref={chartRef} {...obj} />
+  )
+}
+
+export default Echart
